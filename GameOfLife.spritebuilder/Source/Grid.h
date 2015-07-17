a6cca6de562f000c99da00c6145ad2bc4865d633
Totor Ledeb <totorledeb@Totors-Mac-mini.local>
2015-07-17 09:32:32 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Source/Grid.h
@@ -0,0 +1,13 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Totor Ledeb on 17/07/15.
+//  Copyright © 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@end
---
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -0,0 +1,13 @@
+//
+//  Grid.m
+//  GameOfLife
+//
+//  Created by Totor Ledeb on 17/07/15.
+//  Copyright © 2015 Apportable. All rights reserved.
+//
+
+#import "Grid.h"
+
+@implementation Grid
+
+@end
